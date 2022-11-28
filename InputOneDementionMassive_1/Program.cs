1f4f45282ebe14caf7d8f4c8dbadd8d9fb02a7ae
студент <stud@dc.nosu>
2022-11-28 11:07:53 +0300

    Добавьте файлы проекта.

---
+++ InputOneDementionMassive_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOneDementionMassive_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //константный массив
+            int[] mas = { 5, 8, 1, 9, 78 };
+            foreach (var item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
